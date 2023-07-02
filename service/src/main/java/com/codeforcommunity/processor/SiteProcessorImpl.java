87ddff59efd98d2b30b5089646c2332eb4d0ea3b
Garamon <ollietoh@gmail.com>
2023-07-02 00:18:55 -0400

    make sure to get latest site entry

--- service/src/main/java/com/codeforcommunity/processor/SiteProcessorImpl.java
+++ service/src/main/java/com/codeforcommunity/processor/SiteProcessorImpl.java
@@ -239,8 +239,10 @@ public class SiteProcessorImpl implements ISiteProcessor {
   public TreeBenefitsResponse calculateBenefits(int siteId) {
     checkSiteExists(siteId);
 
-    SiteEntriesRecord record =
-        db.selectFrom(SITE_ENTRIES).where(SITE_ENTRIES.SITE_ID.eq(siteId)).fetchOne();
+    SiteEntriesRecord record = db.selectFrom(SITE_ENTRIES)
+            .where(SITE_ENTRIES.SITE_ID.eq(siteId))
+            .orderBy(SITE_ENTRIES.CREATED_AT.desc())
+            .fetchOne();
 
     if (record == null) {
       throw new ResourceDoesNotExistException(siteId, "site entry");
